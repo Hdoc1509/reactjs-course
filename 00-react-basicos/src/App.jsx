2483186e5c64871db473ecec4b6325558a22f4c1
hdoc <blueguitarist1520@gmail.com>
2023-03-01 21:22:43 -0500

    class 16: useState hook

--- 00-react-basicos/src/App.jsx
+++ 00-react-basicos/src/App.jsx
@@ -10,6 +10,7 @@ import ComunicacionComponentes from "./components/ComunicacionComponentes";
 import "./App.css";
 import CicloVida from "./components/CicloVida";
 import AjaxApis from "./components/AjaxApis";
+import ContadorHooks from "./components/ContadorHooks";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -88,6 +89,8 @@ function App() {
         <CicloVida />
         <hr />
         <AjaxApis />
+        <hr />
+        <ContadorHooks titulo="Seguidores" />
       </section>
     </>
   );
