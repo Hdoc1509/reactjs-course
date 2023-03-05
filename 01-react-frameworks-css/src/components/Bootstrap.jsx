bee1b607a9683b382c1eaf31b2bb7d35d698ca48
hdoc <blueguitarist1520@gmail.com>
2023-03-05 18:35:50 -0500

    class 25: frameworks css with react

--- 01-react-frameworks-css/src/components/Bootstrap.jsx
+++ 01-react-frameworks-css/src/components/Bootstrap.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="index.html">
+        <a className="navbar-brand" href="#">
           Navbar
         </a>
         <button
@@ -21,38 +21,33 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="index.html"
-              >
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="index.html">
+              <a className="nav-link" href="#">
                 Link
               </a>
             </li>
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
-                href="index.html"
-                id="navbarDropdown"
+                href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
                 Dropdown
               </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="index.html">
+                  <a className="dropdown-item" href="#">
                     Action
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="index.html">
+                  <a className="dropdown-item" href="#">
                     Another action
                   </a>
                 </li>
@@ -60,16 +55,14 @@ function NavBar() {
                   <hr className="dropdown-divider" />
                 </li>
                 <li>
-                  <a className="dropdown-item" href="index.html">
+                  <a className="dropdown-item" href="#">
                     Something else here
                   </a>
                 </li>
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="index.html">
-                Disabled
-              </a>
+              <a className="nav-link disabled">Disabled</a>
             </li>
           </ul>
           <form className="d-flex" role="search">
@@ -93,7 +86,7 @@ export default function Bootstrap() {
   return (
     <>
       <h2>Bootstrap</h2>
-      <NavBar />
+      <Navbar />
     </>
   );
 }
