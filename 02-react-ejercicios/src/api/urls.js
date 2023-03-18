aa4eb1168aee4adf2713afad81da53cb49f4d90b
hdoc <blueguitarist1520@gmail.com>
2023-03-17 19:22:47 -0500

    refactor: remove unused apis

--- 02-react-ejercicios/src/api/urls.js
+++ 02-react-ejercicios/src/api/urls.js
@@ -1,11 +1,5 @@
 export const HEROKU_CORS = "https://cors-anywhere.herokuapp.com/";
 
-export const THE_AUDIODB_API_URL =
-  "https://www.theaudiodb.com/api/v1/json/2/search.php";
-
-export const MUSIXMATCH_API_URL = "https://api.musixmatch.com/ws/1.1";
-export const MUSIXMATCH_API_CORS_URL = `${HEROKU_CORS}${MUSIXMATCH_API_URL}`;
-
 export const LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0'
 
 export const SOMERANDOMAPI_URL = 'https://some-random-api.ml'
