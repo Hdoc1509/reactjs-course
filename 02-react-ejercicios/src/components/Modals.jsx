192b59acbc8de5c8c7b18af5453f49f1744463f4
hdoc <blueguitarist1520@gmail.com>
2023-03-27 21:57:06 -0500

    class 49-51: modal window

---
+++ 02-react-ejercicios/src/components/Modals.jsx
@@ -0,0 +1,43 @@
+import { useModal } from "../hooks/useModal";
+import ContactForm from "./ContactForm";
+import Modal from "./Modal";
+import SongSearch from "./SongSearch";
+
+const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(true);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+  const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
+
+  return (
+    <div>
+      <h2>Modales</h2>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+        <h3>Modal 1</h3>
+        <p>Hola, este es el contenido de mi modal 1</p>
+        <img src="https://placeimg.com/400/400/animals" alt="Animals" />
+      </Modal>
+
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+        <h3>Otro Modal</h3>
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat.
+        </p>
+        <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+      </Modal>
+      <button onClick={openModalContact}> Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+        <ContactForm />
+      </Modal>
+      <button onClick={openModalSong}>Modal Canciones</button>
+      <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
+        <SongSearch />
+      </Modal>
+    </div>
+  );
+};
+
+export default Modals;
