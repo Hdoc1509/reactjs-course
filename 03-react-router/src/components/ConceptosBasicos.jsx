7965a97e927ccf8c2bd279410dedbc543e2f457f
hdoc <blueguitarist1520@gmail.com>
2023-03-29 17:15:47 -0500

    class 54: react router - syntax for declaring routes

--- 03-react-router/src/components/ConceptosBasicos.jsx
+++ 03-react-router/src/components/ConceptosBasicos.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Acerca from "../pages/Acerca";
+import Contacto from "../pages/Contacto";
 
 const ConceptosBasicos = () => {
   return (
@@ -11,11 +13,47 @@ const ConceptosBasicos = () => {
             <p>Bienvenidos al tema de las Rutas en React</p>
           </Route>
           <Route path="/acerca" exact>
-            <h3>Acerca</h3>
+            <Acerca />
+            <p>
+              Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+              reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+              ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+              Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet
+              voluptate voluptate dolor minim nulla est proident. Nostrud
+              officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
+              occaecat reprehenderit commodo officia dolor Lorem duis laboris
+              cupidatat officia voluptate. Culpa proident adipisicing id nulla
+              nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
+              reprehenderit commodo ex non excepteur duis sunt velit enim.
+              Voluptate laboris sint cupidatat ullamco ut ea consectetur et est
+              culpa et culpa duis.
+            </p>
           </Route>
-          <Route path="/contacto" exact>
-            <h3>Contacto</h3>
-          </Route>
+          {/*<Route path="/contacto" exact component={Contacto} />*/}
+          <Route
+            path="/contacto"
+            exact
+            children={
+              <>
+                <Contacto />
+                <p>
+                  Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+                  reprehenderit enim labore culpa sint ad nisi Lorem pariatur
+                  mollit ex esse exercitation amet. Nisi anim cupidatat
+                  excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem
+                  est aliquip amet voluptate voluptate dolor minim nulla est
+                  proident. Nostrud officia pariatur ut officia. Sit irure elit
+                  esse ea nulla sunt ex occaecat reprehenderit commodo officia
+                  dolor Lorem duis laboris cupidatat officia voluptate. Culpa
+                  proident adipisicing id nulla nisi laboris ex in Lorem sunt
+                  duis officia eiusmod. Aliqua reprehenderit commodo ex non
+                  excepteur duis sunt velit enim. Voluptate laboris sint
+                  cupidatat ullamco ut ea consectetur et est culpa et culpa
+                  duis.
+                </p>
+              </>
+            }
+          />
         </Switch>
       </Router>
     </div>
