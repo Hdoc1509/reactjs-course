84e561b4ecae2b40c23456d2ff434df9c9e13651
hdoc <blueguitarist1520@gmail.com>
2023-04-05 18:30:15 -0500

    class 63: HashRouter

--- 03-react-router/src/components/ConceptosBasicos.jsx
+++ 03-react-router/src/components/ConceptosBasicos.jsx
@@ -1,4 +1,10 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Acerca from "../pages/Acerca";
 import Contacto from "../pages/Contacto";
 import Dashboard from "../pages/Dashboard";
@@ -14,26 +20,51 @@ import PrivateRoute from "./PrivateRoute";
 const ConceptosBasicos = () => {
   return (
     <div>
+      <h2>Hash Router</h2>
+      <HashRouter>
+        <MenuConceptos />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/acerca" component={Acerca} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/usuario/:username" component={Usuario} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/about">
+            <Redirect to="/acerca" />
+          </Route>
+          <Route exact path="/contact">
+            <Redirect to="contacto" />
+          </Route>
+          <Route path="/react" component={ReactTopics} />
+          <Route exact path="/login" component={Login} />
+          {/*<Route exact path="/dashboard" component={Dashboard} />*/}
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </HashRouter>
+      <hr />
       <h2>Conceptos Básicos</h2>
-      <MenuConceptos />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/acerca" component={Acerca} />
-        <Route exact path="/contacto" component={Contacto} />
-        <Route exact path="/usuario/:username" component={Usuario} />
-        <Route exact path="/productos" component={Productos} />
-        <Route exact path="/about">
-          <Redirect to="/acerca" />
-        </Route>
-        <Route exact path="/contact">
-          <Redirect to="contacto" />
-        </Route>
-        <Route path="/react" component={ReactTopics} />
-        <Route exact path="/login" component={Login} />
-        {/*<Route exact path="/dashboard" component={Dashboard} />*/}
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        <Route path="*" component={Error404} />
-      </Switch>
+      <Router>
+        <MenuConceptos />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/acerca" component={Acerca} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/usuario/:username" component={Usuario} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/about">
+            <Redirect to="/acerca" />
+          </Route>
+          <Route exact path="/contact">
+            <Redirect to="contacto" />
+          </Route>
+          <Route path="/react" component={ReactTopics} />
+          <Route exact path="/login" component={Login} />
+          {/*<Route exact path="/dashboard" component={Dashboard} />*/}
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </Router>
     </div>
   );
 };
