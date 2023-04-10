a471624db8806fa5c7df4de6aa70030c151ad649
hdoc <blueguitarist1520@gmail.com>
2023-04-10 18:38:37 -0500

    refactor: move api urls and api keys to 'config.js' file

--- 03-react-router/src/api/urls.js
+++
@@ -1,9 +0,0 @@
-export const HEROKU_CORS = "https://cors-anywhere.herokuapp.com/";
-
-export const LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0'
-
-export const SOMERANDOMAPI_URL = 'https://some-random-api.ml'
-
-export const SPOTIFY_API_URL = 'https://api.spotify.com/v1'
-export const SPOTIFY_API_FREE_TOKEN_URL = 'https://open.spotify.com/get_access_token'
-export const SPOTIFY_API_FREE_TOKEN_CORS_URL = `${HEROKU_CORS}${SPOTIFY_API_FREE_TOKEN_URL}`
--- 03-react-router/src/main.jsx
+++ 03-react-router/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { SPOTIFY_API_FREE_TOKEN_CORS_URL } from "./api/urls";
+import { SPOTIFY_API_FREE_TOKEN_CORS_URL } from "./config";
 import App from "./App";
 import { helpHttp } from "./helpers/helpHttp";
 import "./index.css";
@@ -18,4 +18,3 @@ helpHttp()
     //console.log(tokenData)
     sessionStorage.setItem("spotify-token", tokenData.accessToken);
   });
-
