7ec994ab3c1f5fb997f8636aaf4bb37ce509d5aa
hdoc <blueguitarist1520@gmail.com>
2023-04-15 18:50:54 -0500

    class 79: context api - app dark/light theme with Context

---
+++ 05-react-context/src/components/MainContext.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+const MainContext = ({ texts, auth }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <main className={theme}>
+      <p>{auth ? texts.mainHello : texts.mainWelcome}</p>
+      <p>{texts.mainContent}</p>
+    </main>
+  );
+};
+
+export default MainContext;
