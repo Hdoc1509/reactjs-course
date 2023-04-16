71b145f9b5b1f5d11eb83e526c1e9d2cc19feff6
hdoc <blueguitarist1520@gmail.com>
2023-04-16 18:34:20 -0500

    context api exercise - app with use session with Context

--- 05-react-context/src/components/MyPageContext.jsx
+++ 05-react-context/src/components/MyPageContext.jsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
+import { AuthProvider } from "../context/AuthContext";
 import { LanguageProvider } from "../context/LanguageContext";
 import { ThemeProvider } from "../context/ThemeContext";
 import FooterContext from "./FooterContext";
 import HeaderContext from "./HeaderContext";
 import MainContext from "./MainContext";
 
-const initialAuth = null;
-
 const MyPageContext = () => {
-  const [auth, setAuth] = useState(initialAuth);
-
-  const handleAuth = () => {
-    if (auth) {
-      setAuth(null);
-    } else {
-      setAuth(true);
-    }
-  };
-
   return (
     <div className="my-page">
       <ThemeProvider>
         <LanguageProvider>
-          <HeaderContext auth={auth} handleAuth={handleAuth} />
-          <MainContext auth={auth} />
-          <FooterContext />
+          <AuthProvider>
+            <HeaderContext />
+            <MainContext />
+            <FooterContext />
+          </AuthProvider>
         </LanguageProvider>
       </ThemeProvider>
     </div>
